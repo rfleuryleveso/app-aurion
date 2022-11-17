7128120d421d0f2b7b02f01147545affd60e9ce8
Raphaël Fleury-le veso <mail@rflv.fr>
2022-11-17 18:33:19 +0100

    Delete unused mainActivity

--- app/src/main/java/fr/rflv/appaurion/services/aurion/impl/AurionImpl.kt
+++ app/src/main/java/fr/rflv/appaurion/services/aurion/impl/AurionImpl.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import fr.rflv.appaurion.services.aurion.interfaces.IAurion
 import fr.rflv.appaurion.services.aurion.models.ICourse
 import fr.rflv.appaurion.services.aurion.models.IMark
+import fr.rflv.appaurion.services.database.CoursesDatabaseHelper
 import org.koin.core.annotation.Single
 
 
 @Single
-open class AurionImpl(private val appContext: Context) : IAurion {
+open class AurionImpl(private val appContext: Context, private val coursesDatabaseHelper: CoursesDatabaseHelper) : IAurion {
 
     override fun getAllCourses(): ArrayList<ICourse> {
         return ArrayList<ICourse>();
