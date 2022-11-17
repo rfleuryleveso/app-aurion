0915ef80b8e31ac5eb702d47f1a13402e9370f3d
Jambon <armand.deffrennes@student.junia.com>
2022-11-17 19:48:51 +0100

    Implemented lifecycleScope

--- app/src/main/java/fr/rflv/appaurion/ScheduleActivity.kt
+++ app/src/main/java/fr/rflv/appaurion/ScheduleActivity.kt
@@ -4,10 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import fr.rflv.appaurion.viewmodels.LoginViewModel
+import fr.rflv.appaurion.viewmodels.ScheduleViewModel
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ScheduleActivity : AppCompatActivity() {
     private lateinit var deconnectionImage : ImageView
     private lateinit var gradesImage : ImageView
+    val scheduleViewModel by viewModel<ScheduleViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +29,16 @@ class ScheduleActivity : AppCompatActivity() {
         }
         gradesImage.setOnClickListener() {
             changeToGradesActivity()
+        }
+
+        lifecycleScope.launch(){
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                scheduleViewModel.uiState.collect {
+                    for (course in it.courses) {
+                        //Create a fragment for each course
+                    }
+                }
+            }
         }
     }
 
