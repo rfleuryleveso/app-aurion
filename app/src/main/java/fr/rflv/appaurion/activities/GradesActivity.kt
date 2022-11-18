13f8cc4c03d240634f2672ab9495ae128ac80392
Marie Pivette <marie.pivette@student.yncrea.fr>
2022-11-18 13:11:51 +0100

    last push

--- app/src/main/java/fr/rflv/appaurion/activities/GradesActivity.kt
+++ app/src/main/java/fr/rflv/appaurion/activities/GradesActivity.kt
@@ -4,13 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import fr.rflv.appaurion.R
 import fr.rflv.appaurion.fragments.GradesRecycleViewFragment
+import java.util.*
 
 class GradesActivity : AppCompatActivity() {
     private lateinit var deconnectionImage : ImageView
     private lateinit var scheduleImage : ImageView
+
+    private lateinit var dateOfWeek: TextView
+    private lateinit var day: TextView
+    private lateinit var monthAndYear: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +25,18 @@ class GradesActivity : AppCompatActivity() {
 
         deconnectionImage = findViewById(R.id.deconnection)
         scheduleImage = findViewById(R.id.calendar)
+
+        dateOfWeek = findViewById(R.id.dayDate)
+        day = findViewById(R.id.day)
+        monthAndYear = findViewById(R.id.month)
+
+        var days = listOf("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi")
+        var months = listOf("Janv.","Févr.","Mars","Avr.","Mai","Juin","Juill.","Août","Sept.","Oct.","Nov.","Déc.")
+        val date = Calendar.getInstance()
+        dateOfWeek.text = date.get(Calendar.DAY_OF_MONTH).toString()
+        day.text = days[date.get(Calendar.DAY_OF_WEEK) - 1]
+        monthAndYear.text = months[date.get(Calendar.MONTH)] + " " + date.get(Calendar.YEAR)
+
 
         deconnectionImage.setOnClickListener() {
             finish()
